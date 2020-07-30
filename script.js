c530b0bddf941888c108d618fc273c64623d52d3
leo848 <leoblume@gmx.de>
2020-07-30 12:42:27 +0200

    initial

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var rcf = undefined;
 var game = new Chess();
 
 var board = null;
+var isAbleToMove = true;
 
 function onDragStart (
 	source,
@@ -20,15 +21,16 @@ function onDragStart (
 	// do not pick up pieces if the game is over
 	if (game.game_over()) return false;
 
+	if (!isAbleToMove) return false;
+
 	// only pick up pieces for the side to move
 	if (
 		(game.turn() === 'w' &&
 			piece.search(/^b/) !== -1) ||
 		(game.turn() === 'b' &&
 			piece.search(/^w/) !== -1)
-	) {
+	)
 		return false;
-	}
 }
 
 function onDrop (source, target){
@@ -67,8 +69,24 @@ function updateStatus (){
 			' is in checkmate.';
 		setTimeout(nextIndex, 500);
 	} else {
-		game.undo();
-		board.position(game.fen());
+		isAbleToMove = false;
+
+		board.position(game.fen(), false);
+		game.move(
+			game.moves()[
+				Math.floor(
+					Math.random() *
+						game.moves().length,
+				)
+			],
+		);
+
+		setTimeout(() => {
+			game.undo();
+			game.undo();
+			board.position(game.fen());
+			isAbleToMove = true;
+		}, 1000);
 	}
 
 	// $status.html(status)
@@ -89,8 +107,13 @@ updateStatus();
 
 function nextIndex (){
 	rcf = randomChessFensIndex();
+	$('#fen-id').html(rcf);
+
 	game.load(FENS[rcf]);
 	board.position(FENS[rcf]);
+	$('#side-to-move').html(
+		game.turn() == 'w' ? 'Weiß' : 'Schwarz',
+	);
 	board.orientation(
 		game.turn() == 'w' ? 'white' : 'black',
 	);
