180197adbf4edfb5c3da7a6ca38fb79345c9b7d9
leo848 <58192100+leo848@users.noreply.github.com>
2020-09-04 08:19:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -202,7 +202,7 @@
 	$('#nextButton').on('click', nextIndex);
 	
 	function splash_screen(){
-		document.body.innerHTML += "<div>Gut gemacht!</div>
+		document.body.innerHTML += "<div style="">Gut gemacht!</div>;
 	
 	};
 })();
