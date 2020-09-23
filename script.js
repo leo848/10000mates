6d1aa4fd1e2eb9921d54e9155dd72292a95ffaae
Jonas Leonardo Blume <J.Blume@schulen.essen.de>
2020-09-23 11:05:17 +0200

    sweetalert!

--- script.js
+++ script.js
@@ -1,212 +1,224 @@
-(function (){
+(function () {
 	try {
-	document.body.addEventListener('touchstart', function(e){ e.preventDefault(); });
-	var currentParams = (function (url){
-		var params = {};
-		var parser = document.createElement('a');
-		parser.href = url;
-		var query = parser.search.substring(1);
-		var vars = query.split('&');
-		for (var i = 0; i < vars.length; i++) {
-			var pair = vars[i].split('=');
-			params[pair[0]] = decodeURIComponent(
-				pair[1],
-			);
-		}
-		return params;
-	})(window.location.url);
+		document.body.addEventListener('touchstart', function (e) {
+			e.preventDefault();
+		});
 
-	if ('fen' in currentParams) {
-		document.body.innerHTML = `${FENS[
+		let score = 0;
+
+		var currentParams = (function (url) {
+			var params = {};
+			var parser = document.createElement('a');
+			parser.href = url;
+			var query = parser.search.substring(1);
+			var vars = query.split('&');
+			for (var i = 0; i < vars.length; i++) {
+				var pair = vars[i].split('=');
+				params[pair[0]] = decodeURIComponent(
+					pair[1],
+				);
+			}
+			return params;
+		})(window.location.url);
+
+		if ('fen' in currentParams) {
+			document.body.innerHTML = `${FENS[
 			currentParams.fen
 		]}`;
-		return;
-	}
-	function randomChessFensIndex (){
-		return Math.floor(
-			Math.random() * FENS.length,
-		);
-	}
+			return;
+		}
 
-	//===========================================================\\
-	var rcf = undefined;
+		function randomChessFensIndex() {
+			return Math.floor(
+				Math.random() * FENS.length,
+			);
+		}
 
-	var game = new Chess();
+		//===========================================================\\
+		var rcf = undefined;
 
-	var board = null;
-	var isAbleToMove = true;
+		var game = new Chess();
 
-	var time = 60;
+		var board = null;
+		var isAbleToMove = true;
+		var splash = true
 
-	let timerInterval = setInterval(() => {
-		if (time >= 0) {
-			time -= 0.1;
-			$('#bar').css(
-				'width',
-				Math.round(time * 10) /
+		var time = 60;
+
+		let timerInterval = setInterval(() => {
+			if (time >= 0) {
+				time -= 0.1;
+				$('#bar').css(
+					'width',
+					Math.round(time * 10) /
 					10 *
 					(100 / 60) +
 					'%',
-			);
-			$('#bar').css(
-				'background-color',
-				`hsl(${time * 2.4}, 100%, 50%)`,
-				// 'hsl(' +
-				// 	 +
-				// 	', 100, 50)',
-			);
-		} else {
-		isAbleToMove = false;
-			splash_screen();
-		}
-	}, 100);
+				);
+				$('#bar').css(
+					'background-color',
+					`hsl(${time * 2.4}, 100%, 50%)`,
+					// 'hsl(' +
+					// 	 +
+					// 	', 100, 50)',
+				);
+			} else {
+				isAbleToMove = false;
+				if (splash) {
+					splash_screen();
+					splash = false
+				}
 
-	
-	
-	
-	function onDragStart (
-		source,
-		piece,
-		position,
-		orientation,
-	){
-		// do not pick up pieces if the game is over
-		if (game.game_over()) return false;
+			}
+		}, 100);
 
-		if (!isAbleToMove) return false;
 
-		// only pick up pieces for the side to move
-		if (
-			(game.turn() === 'w' &&
-				piece.search(/^b/) !== -1) ||
-			(game.turn() === 'b' &&
-				piece.search(/^w/) !== -1)
+
+
+		function onDragStart(
+			source,
+			piece,
+			position,
+			orientation,
 		) {
-			makeStatusTextBold(250);
-			return false;
-		}
-	}
+			// do not pick up pieces if the game is over
+			if (game.game_over()) return false;
 
-	function onDrop (source, target){
-		// see if the move is legal
-		var move = game.move({
-			from      : source,
-			to        : target,
-			promotion : 'q', // NOTE: always promote to a queen for example simplicity
-		});
+			if (!isAbleToMove) return false;
 
-		// illegal move
-		if (move === null) return 'snapback';
-
-		updateStatus();
-	}
-
-	// update the board position after the piece snap
-	// for castling, en passant, pawn promotion
-	function onSnapEnd (){
-		board.position(game.fen());
-	}
-
-	function updateStatus (){
-		var status = '';
-
-		var moveColor = 'White';
-		if (game.turn() === 'b') {
-			moveColor = 'Black';
+			// only pick up pieces for the side to move
+			if (
+				(game.turn() === 'w' &&
+					piece.search(/^b/) !== -1) ||
+				(game.turn() === 'b' &&
+					piece.search(/^w/) !== -1)
+			) {
+				makeStatusTextBold(250);
+				return false;
+			}
 		}
 
-		// checkmate?
-		if (game.in_checkmate()) {
-			status =
-				'Game over, ' +
-				moveColor +
-				' is in checkmate.';
-			$('#score').html(
-				$('#score').html() - -1,
-			);
-			setTimeout(nextIndex, 500);
-		} else {
-			isAbleToMove = false;
+		function onDrop(source, target) {
+			// see if the move is legal
+			var move = game.move({
+				from: source,
+				to: target,
+				promotion: 'q', // NOTE: always promote to a queen for example simplicity
+			});
 
-			board.position(game.fen(), false);
-			game.move(
-				game.moves()[
-					Math.floor(
-						Math.random() *
+			// illegal move
+			if (move === null) return 'snapback';
+
+			updateStatus();
+		}
+
+		// update the board position after the piece snap
+		// for castling, en passant, pawn promotion
+		function onSnapEnd() {
+			board.position(game.fen());
+		}
+
+		function updateStatus() {
+			var status = '';
+
+			var moveColor = 'White';
+			if (game.turn() === 'b') {
+				moveColor = 'Black';
+			}
+
+			// checkmate?
+			if (game.in_checkmate()) {
+				status =
+					'Game over, ' +
+					moveColor +
+					' is in checkmate.';
+				$('#score').html(
+					++score,
+				);
+				setTimeout(nextIndex, 500);
+			} else {
+				isAbleToMove = false;
+
+				board.position(game.fen(), false);
+				game.move(
+					game.moves()[
+						Math.floor(
+							Math.random() *
 							game.moves().length,
-					)
-				],
-			);
+						)
+					],
+				);
 
-			setTimeout(() => {
-				game.undo();
-				game.undo();
-				board.position(game.fen());
-				isAbleToMove = true;
-			}, 1000);
+				setTimeout(() => {
+					game.undo();
+					game.undo();
+					board.position(game.fen());
+					isAbleToMove = true;
+				}, 1000);
+			}
+
+			// $status.html(status)
+			// $fen.html(game.fen())
+			// $pgn.html(game.pgn())
 		}
 
-		// $status.html(status)
-		// $fen.html(game.fen())
-		// $pgn.html(game.pgn())
-	}
-
-	function makeStatusTextBold (time){
-		$('#side-to-move').css(
-			'font-weight',
-			'bold',
-		);
-		setTimeout(() => {
+		function makeStatusTextBold(time) {
 			$('#side-to-move').css(
 				'font-weight',
-				'normal',
+				'bold',
 			);
-		}, time);
-	}
+			setTimeout(() => {
+				$('#side-to-move').css(
+					'font-weight',
+					'normal',
+				);
+			}, time);
+		}
 
-	var config = {
-		draggable   : true,
-		position    : 'start',
-		onDragStart : onDragStart,
-		onDrop      : onDrop,
-		onSnapEnd   : onSnapEnd,
-	};
-	board = new Chessboard('myBoard', config);
+		var config = {
+			draggable: true,
+			position: 'start',
+			onDragStart: onDragStart,
+			onDrop: onDrop,
+			onSnapEnd: onSnapEnd,
+		};
+		board = new Chessboard('myBoard', config);
 
-	updateStatus();
+		updateStatus();
 
-	function nextIndex (){
-		rcf = randomChessFensIndex();
-		$('#fen-id').html(rcf);
+		function nextIndex() {
+			rcf = randomChessFensIndex();
+			$('#fen-id').html(rcf);
 
-		game.load(FENS[rcf]);
-		board.position(FENS[rcf]);
-		$('#side-to-move').html(
-			game.turn() == 'w'
-				? 'Weiß'
-				: 'Schwarz',
-		);
-		$('#solution').attr(
-			'href',
-			'https://lichess.org/analysis/' +
+			game.load(FENS[rcf]);
+			board.position(FENS[rcf]);
+			$('#side-to-move').html(
+				game.turn() == 'w' ?
+				'Weiß' :
+				'Schwarz',
+			);
+			$('#solution').attr(
+				'href',
+				'https://lichess.org/analysis/' +
 				game.fen().replace(/ /gi, '_'),
-		);
-		board.orientation(
-			game.turn() == 'w'
-				? 'white'
-				: 'black',
-		);
+			);
+			board.orientation(
+				game.turn() == 'w' ?
+				'white' :
+				'black',
+			);
+		}
+
+		nextIndex();
+
+		$('#nextButton').on('click', nextIndex);
+		$('#splash').on('click', splash_screen);
+
+		function splash_screen() {
+			Swal.fire(`${score}`, "Lol", "success")
+
+		};
+	} catch (e) {
+		alert(e)
 	}
-
-	nextIndex();
-
-	$('#nextButton').on('click', nextIndex);
-	$('#splash').on('click', splash_screen);
-	
-	function splash_screen(){
-		document.body.innerHTML += '<div id="splashscreen">Gut gemacht!</div>';
-	
-	};}
-	catch (e) {
-	alert(e)}
 })();
